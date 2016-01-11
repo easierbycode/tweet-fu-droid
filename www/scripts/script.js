c6fffe8fa1ddf3c0f3060f36d436de7651734e73
easierbycode <daniel@easierbycode.com>
2016-01-11 22:36:57 +0000

    initial stuff

---
+++ www/scripts/script.js
@@ -0,0 +1,16 @@
+$(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://twittersearchapi.herokuapp.com/search',
+        success: function(response) {
+            var resObj = JSON.parse(response);
+            $.each(resObj, function(index, value) {
+                $('#ulTweets').append('<li class="list-group-item"><span class="badge">' + resObj[index].retweet_count + '</span>' + resObj[index].text + '</li>');
+
+            });
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+});
